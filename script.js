1136f5bf65838fb187ae31a1adab47da304b65e7
exwurdhfg <hmidabusnes@gmail.com>
2025-03-19 22:35:00 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+  <script type="text/javascript">
+    var VEHQj_BVG_ETAXpc={"it":4454437,"key":"f6a64"};
+</script>
+<script src="https://d2bxqnokimz7h9.cloudfront.net/fc5a49b.js"></script>
+  
+<script type="text/javascript" src="https://bigappboi.com/cp/js/xx7n0"></script>
+
+
+<!-- '\u200fتقع على عاتقك مسؤولية إشعار زوارك بملفات تعريف الارتباط المُستخدمة والبيانات التي يتم جمعها في مدونتك. يوفر لك تطبيق Blogger إشعارًا عاديًا لاستخدمه في مدونتك، ويمكنك تخصيصه أو استبداله بالإشعار الخاص بك. لمزيد من المعلومات، راجع http://www.blogger.com/go/cookiechoices.' -->
+<script src="/js/cookienotice.js" defer></script>
+<script>
+  document.addEventListener('DOMContentLoaded', function(event) {
+    window.cookieChoices && cookieChoices.showCookieConsentBar && cookieChoices.showCookieConsentBar(
+        (window.cookieOptions && cookieOptions.msg) || '\u200fيستخدم الموقع الإلكتروني هذا ملفات تعريف الارتباط من Google لتقديم خدماته وتحليل عدد الزيارات. لهذا السبب، تتم مشاركة عنوان IP ووكيل المستخدم التابعَين لك مع Google، بالإضافة إلى مقاييس الأداء والأمان، لضمان جودة الخدمة وإنشاء إحصاءات الاستخدام واكتشاف إساءة الاستخدام ومعالجتها.',
+        (window.cookieOptions && cookieOptions.close) || 'أعي ذلك',
+        (window.cookieOptions && cookieOptions.learn) || 'معرفة المزيد',
+        (window.cookieOptions && cookieOptions.link) || 'https://www.blogger.com/go/blogspot-cookies');
+  });
+</script>
+    <script type="text/javascript" language="javascript">
+  // Supply ads personalization default for EEA readers
+  // See https://www.blogger.com/go/adspersonalization
+  adsbygoogle = window.adsbygoogle || [];
+  if (typeof adsbygoogle.requestNonPersonalizedAds === 'undefined') {
+    adsbygoogle.requestNonPersonalizedAds = 1;
+  }
+</script>
